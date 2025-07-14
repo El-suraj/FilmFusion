3369d8360767c52143b8418ee57f3d0740149d91
El-suraj <surajohussaini7@gmail.com>
2025-07-14 13:12:32 +0100

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import '../app.css';
+import '../App.css';
 
 function Navbar() {
   const { user, logout } = useAuth();
